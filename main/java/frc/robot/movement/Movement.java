69ffb1432dd7e3f9961723583111d8abc8faafd0
Alex Ament <alexcament@gmail.com>
2024-01-09 13:58:53 -0500

    add PWMSparkMax objects

--- main/java/frc/robot/movement/Movement.java
+++ main/java/frc/robot/movement/Movement.java
@@ -2,6 +2,19 @@ package frc.robot.movement;
 
 public class Movement {
 
+    /* These objects are the Pulse Width Modulation (PWM) controllers. They are how
+     * the RoboRIO controls the motors. The _drive objects control the actual drive;
+     * the _swerve objects control the direction of the corresponding wheel.
+    */
+    private final PWMSparkMax left_front_drive = new PWMSparkMax(0);
+    private final PWMSparkMax left_front_swerve = new PWMSparkMax(1);
+    private final PWMSparkMax left_back_drive = new PWMSparkMax(2);
+    private final PWMSparkMax left_back_swerve = new PWMSparkMax(3);
+    private final PWMSparkMax right_front_drive = new PWMSparkMax(4);
+    private final PWMSparkMax right_front_swerve = new PWMSparkMax(5);
+    private final PWMSparkMax right_back_drive = new PWMSparkMax(6);
+    private final PWMSparkMax right_back_swerve = new PWMSparkMax(7);
+
     // TODO: add unit for moveTime parameter
     /**
      * Moves the robot in a given direction for a given amount of time.
