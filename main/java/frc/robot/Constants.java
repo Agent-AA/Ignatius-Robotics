ea05057e8bfa705bec928aae0558dce7fe8a4e35
Ben Beredo <bcberedo17@gmail.com>
2024-01-17 19:19:19 -0500

    Implemented a command-based structure for moving the robot into the code. This included adding modules for the wheels, various constants, and a subsystem for driving.
    Most of this code is from the SwerveControllerCommand FRC project.

--- main/java/frc/robot/Constants.java
+++ main/java/frc/robot/Constants.java
@@ -5,15 +5,94 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class holds robot-wide numerical or boolean constants.
+ * This class should not be used for any other purpose. All constants should be declared
+ * globally. Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  // Constants used by the DriveSubsystem Subsystem.
+  // These need to be determined for our particular robot.
+  public static final class DriveConstants {
+    public static final int kFrontLeftDriveMotorPort = 0;
+    public static final int kRearLeftDriveMotorPort = 2;
+    public static final int kFrontRightDriveMotorPort = 4;
+    public static final int kRearRightDriveMotorPort = 6;
+
+    public static final int kFrontLeftTurningMotorPort = 1;
+    public static final int kRearLeftTurningMotorPort = 3;
+    public static final int kFrontRightTurningMotorPort = 5;
+    public static final int kRearRightTurningMotorPort = 7;
+
+    public static final int[] kFrontLeftTurningEncoderPorts = new int[] {0, 1};
+    public static final int[] kRearLeftTurningEncoderPorts = new int[] {2, 3};
+    public static final int[] kFrontRightTurningEncoderPorts = new int[] {4, 5};
+    public static final int[] kRearRightTurningEncoderPorts = new int[] {6, 7};
+
+    public static final boolean kFrontLeftTurningEncoderReversed = false;
+    public static final boolean kRearLeftTurningEncoderReversed = true;
+    public static final boolean kFrontRightTurningEncoderReversed = false;
+    public static final boolean kRearRightTurningEncoderReversed = true;
+
+    public static final int[] kFrontLeftDriveEncoderPorts = new int[] {8, 9};
+    public static final int[] kRearLeftDriveEncoderPorts = new int[] {10, 11};
+    public static final int[] kFrontRightDriveEncoderPorts = new int[] {12, 13};
+    public static final int[] kRearRightDriveEncoderPorts = new int[] {14, 15};
+
+    public static final boolean kFrontLeftDriveEncoderReversed = false;
+    public static final boolean kRearLeftDriveEncoderReversed = true;
+    public static final boolean kFrontRightDriveEncoderReversed = false;
+    public static final boolean kRearRightDriveEncoderReversed = true;
+
+    // DriveSubsystem.drive() period
+    public static final double kDrivePeriod = TimedRobot.kDefaultPeriod;
+
+    public static final double kTrackWidth = 0.5; // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = 0.7; // Distance between front and back wheels on robot
+    
+    // Converts between ChassisSpeeds object and SwerveModuleState objects
+    public static final SwerveDriveKinematics kDriveKinematics =
+        new SwerveDriveKinematics(
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    public static final boolean kGyroReversed = false;
+
+    // These characterization values must be determined either experimentally or theoretically
+    // The SysId tool is a convenient method for obtaining these values for the robot
+    public static final double ksVolts = 0;
+    public static final double kvVoltSecondsPerMeter = 0;
+    public static final double kaVoltSecondsSquaredPerMeter = 0;
+
+    public static final double kMaxSpeedMetersPerSecond = 0;    
+  }
+
+  // Constants for the SwerveModule class
+  // CHANGE THESE LATER
+  public static final class ModuleConstants {
+    public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
+    public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
+
+    public static final int kEncoderCPR = 1024;
+    public static final double kWheelDiameterMeters = 0.15;
+    public static final double kDriveEncoderDistancePerPulse =
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+
+    public static final double kTurningEncoderDistancePerPulse =
+        // Assumes the encoders are on a 1:1 reduction with the module shaft.
+        (2 * Math.PI) / (double) kEncoderCPR;
+
+    public static final double kPModuleTurningController = 1;
+
+    public static final double kPModuleDriveController = 1;
   }
 }
