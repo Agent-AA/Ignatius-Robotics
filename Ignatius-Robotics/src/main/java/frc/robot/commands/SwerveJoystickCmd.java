d3b18de892ef5150dde8a063fb435b70dc3ff7de
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-02-13 13:28:35 -0500

    add mr matheou's files

---
+++ Ignatius-Robotics/src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -0,0 +1,109 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+/*    REPLACED BY ALTERNATE IMPORTS (SEE BELOW)
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+*/
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+import edu.wpi.first.wpilibj2.command.Command;  //Changed to Command from CommandBase
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+
+
+
+
+public class SwerveJoystickCmd extends Command {     //Changed to Command from CommandBase
+  
+    private final SwerveSubsystem swerveSubsystem;
+    private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
+    private final Supplier<Boolean> fieldOrientedFunction;
+    private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+
+    public SwerveJoystickCmd(SwerveSubsystem swerveSubsystem,
+            Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> turningSpdFunction,
+            Supplier<Boolean> fieldOrientedFunction) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.xSpdFunction = xSpdFunction;
+        this.ySpdFunction = ySpdFunction;
+        this.turningSpdFunction = turningSpdFunction;
+        this.fieldOrientedFunction = fieldOrientedFunction;
+        this.xLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.yLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.turningLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("SwerveSubsystem Initilizing");
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("SwerveSubsystem Executing");
+        
+        // 1. Get real-time joystick inputs
+        double xSpeed = xSpdFunction.get();
+        double ySpeed = ySpdFunction.get();
+        double turningSpeed = turningSpdFunction.get();
+
+        // 2. Apply deadband
+        xSpeed = Math.abs(xSpeed) > OIConstants.kDeadband ? xSpeed : 0.0;
+        ySpeed = Math.abs(ySpeed) > OIConstants.kDeadband ? ySpeed : 0.0;
+        turningSpeed = Math.abs(turningSpeed) > OIConstants.kDeadband ? turningSpeed : 0.0;
+
+        // 3. Make the driving smoother
+        xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        ySpeed = yLimiter.calculate(ySpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        turningSpeed = turningLimiter.calculate(turningSpeed)
+                * DriveConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond;
+
+        // 4. Construct desired chassis speeds
+        ChassisSpeeds chassisSpeeds;
+        if (fieldOrientedFunction.get()) {
+            // Relative to field
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
+        } else {
+            // Relative to robot
+            chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+        }
+             
+//MANUALLY SETTING SPEEDS BEGIN
+/* 
+        double xSpeed = .25;
+        double ySpeed = .25;
+        double turningSpeed = .25;
+        ChassisSpeeds chassisSpeeds;
+         chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+*/
+//MANUALLY SETTING SPEEDS END
+
+        // 5. Convert chassis speeds to individual module states
+        SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+        // 6. Output each module states to wheels
+        swerveSubsystem.setModuleStates(moduleStates);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("SwerveSubsystem Ending");
+        swerveSubsystem.stopModules();
+    }
+
+    @Override
+    public boolean isFinished() {
+        System.out.println("SwerveSubsystem Finished");
+        return false;
+    }
+}
---
+++ Ignatius-Robotics/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,153 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;   // Used com.revrobotics.CANSparkLowLevel.MotorType; instead
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+//import edu.wpi.first.wpilibj.geometry.Rotation2d;
+//import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+
+
+public class SwerveModule {
+    //private CANSparkMax m_SparkMaxMotor = new CANSparkMax(10, MotorType.kBrushless);
+    //private final CANSparkMax m_CanSparkMax;
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turningMotor;
+
+    //private final CANcoder driveEncoder;   //replaced REV's CANencoder / RelativeEncoder with CTRE's CANcoder 
+    //private final CANcoder turningEncoder; //replaced REV's CANencoder / RelativeEncoder with CTRE's CANcoder 
+
+    private final RelativeEncoder driveEncoder;   //replaced REV's CANencoder with RevRobotics RelativeEncoder 
+    private final RelativeEncoder turningEncoder; //replaced REV's CANencoder  with RevRobotics RelativeEncoder  
+
+    private final PIDController turningPidController;
+
+    //private final AnalogInput absoluteEncoder;  //AnalogInput Encoder Replaced by CANencoder absoluteEncoder  
+    private final CANcoder absoluteEncoder;
+   
+    private final boolean absoluteEncoderReversed;
+    private final double absoluteEncoderOffsetRad;
+//IMPLEMENTION SWERVEMODULE 12:41
+    public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+            int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+
+        this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
+        //absoluteEncoder = new AnalogInput(absoluteEncoderId); //replaced by CTRE CANcoder
+        absoluteEncoder = new CANcoder(absoluteEncoderId);
+       
+
+        //m_CanSparkMax = new CANSparkMax(10,MotorType.kBrushless);
+
+        driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
+        turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
+
+        
+
+        
+        driveMotor.setInverted(driveMotorReversed);
+        turningMotor.setInverted(turningMotorReversed);
+
+        driveEncoder = driveMotor.getEncoder();
+        turningEncoder = turningMotor.getEncoder();
+
+        driveEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter);
+        driveEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+        turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
+        turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+
+      
+
+        turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+        resetEncoders();
+            }
+
+            public double getDrivePosition() {
+        return driveEncoder.getPosition();
+    }
+
+    public double getTurningPosition() {
+        return turningEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return driveEncoder.getVelocity();
+    }
+
+    public double getTurningVelocity() {
+        return turningEncoder.getVelocity();
+    }
+
+   /*  Replaced public double getAbsoluteEncoderRad() function with getAbsoluteEncoderPosition function AND resetEncoders with per the following:
+   https://www.chiefdelphi.com/t/how-to-get-radians-voltage-from-ctre-cancoder/450759/3   due to using CANcoder vs. Analog encoder
+   
+   {
+
+    public double getAbsoluteEncoderRad() {
+        //double angle = absoluteEncoder.getVoltage() / RobotController.getVoltage5V();
+        double angle = absoluteEncoder.getBus() / RobotController.getVoltage5V();
+
+        angle *= 2.0 * Math.PI;
+        angle -= absoluteEncoderOffsetRad;
+        return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+    }
+
+    }
+    public void resetEncoders() {
+        driveEncoder.setPosition(0);
+        turningEncoder.setPosition(getAbsoluteEncoderRad());
+    }
+
+   */
+
+    public double getAbsoluteEncoderPosition() {
+        return absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+    }
+
+    public void resetEncoders() {
+        driveEncoder.setPosition(0);
+        turningEncoder.setPosition(getAbsoluteEncoderPosition());
+    }    
+    
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+        state = SwerveModuleState.optimize(state, getState().angle);
+        driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+        //SmartDashboard.putString("Swerve[" + absoluteEncoder.getChannel() + "] state", state.toString());
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getDeviceID() + "] state", state.toString());  //Modified for CANcoder
+    }
+
+    public void stop() {
+        driveMotor.set(0);
+        turningMotor.set(0);
+    }
+
+
+}
+  
