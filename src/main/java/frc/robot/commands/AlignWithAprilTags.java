e7c1a450e0f49fe92fbd6c8fe11c1277e6034f0d
Ben Beredo <bcberedo17@gmail.com>
2024-02-24 12:39:55 -0500

    Added basic tracking code
    Created subsystem & command that tracks the tv, ta, and tx values of the limelight and posts it to SmartDashboard. Also added LimelightHelpers library to code, just in case we want to use it

---
+++ src/main/java/frc/robot/commands/AlignWithAprilTags.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.SwerveSubsystem; 
+import frc.robot.subsystems.VisionSubsystem; 
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AlignWithAprilTags extends Command {
+    private final SwerveSubsystem m_swerveSubsystem;
+    private final VisionSubsystem m_visionSubsystem;
+
+    /**
+     * Command constructor
+     */
+    public AlignWithAprilTags(SwerveSubsystem swerveSubsystem, VisionSubsystem visionSubsystem) {
+        m_swerveSubsystem = swerveSubsystem;
+        m_visionSubsystem = visionSubsystem;
+        // add Command requirements
+        addRequirements(m_swerveSubsystem, m_visionSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+    
+    }
+}
